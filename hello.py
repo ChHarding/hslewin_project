7d0a10131181e9d6c46054008b9db14f3adfcd8a
Heather Lewin <34514655+hslewin@users.noreply.github.com>
2024-09-14 07:17:35 -0500

    Changing more for HW

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 print("hello")
 print("i forget the quotes around hello [sad]")
 print("I added the license late")
+
+#I am slowly working my way to understanding github better. Practice brings better understanding  
+
