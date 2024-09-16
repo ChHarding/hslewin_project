2399a844ad932029479349b3bcfedc8c07890658
Heather Lewin <34514655+hslewin@users.noreply.github.com>
2024-09-15 19:14:03 -0500

    Create WIP.py
    I am doing homework practice.  I have added a print line and a comment line that contains a question.

---
+++ WIP.py
@@ -0,0 +1,3 @@
+print("I hope that this week is less busy than last week")
+
+#Can I use Figma to sketh out my design ideas and then use the tinker plug-in to get the code from there or do I need to code it all from scratch?
